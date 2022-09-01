29644e6aa3e6253daa404006326d5a067e1fd9d4
stobertonf <stobertonf@gmail.com>
2022-09-01 06:13:52 -0300

    feat(v1): Refactoring The Main Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,125 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessoas",
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key? key)) super(key: key);
+
+
+  @override 
+  Widget build(Buildcontext contexxt) {
+    return MaterialApp(
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
-      home: Home()));
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  int _people = 0;
-  String _infoText = "Teste!";
-
-  void _changePeople(int delta){
-    setState(() {
-      _people += delta;
-
-      if(_people < 0){
-        _infoText = "Mundo invertido?!";
-      } else if(_people <= 10){
-        _infoText = "Pode Entrar!";
-      } else {
-        _infoText = "Lotado!";
-      }
-
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          "images/restaurant.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Pessoas: $_people",
-              style:
-              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ),
-                    onPressed: () {
-                      _changePeople(1);
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text(
-                      "-1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ),
-                    onPressed: () {
-                      _changePeople(-1);
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              _infoText,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0),
-            )
-          ],
-        )
-      ],
     );
+
   }
+
+  class HomePage extends StatefulWidget{
+    @override
+    Widget build(Buildcontext context){
+      return Container();
+    }
+  }
+
+  class _HomePageState extends State<HomePage>{
+    int count = 0;
+
+    void decrement(){
+      setState((){
+        count --;
+      });
+      
+    }
+
+    void incrementCount() {
+      setState((){
+        count ++ ;
+      });
+      
+    }
+
+    return Scaffold(
+    backgroundColor: Colors.red,
+     body:  Container(
+      decoration:  BoxDecoration(
+        image: AssetImage('assets/images/picanha.jpg'),
+        fit: BoxFit.cover,
+      ),
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+           Text(
+              "Pode Entrar",
+              style: TextStyle(
+                fontSize:30,
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+              ),
+           ), 
+            Padding(
+            padding: EdgeInsets.all(40),
+           Text(
+              count.toString(), //Transformando uma variável inteira para string
+              style: const TextStyle(
+                fontSize:100,
+                color: Colors.white,     
+              ),
+            ),
+           ), 
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+     
+            TextButton(
+                 onPressd: decrement,
+                 style: TextButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor:Colors.white,
+                  fixeSized: const Sized(100,100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(16),
+                  ),               
+                 ), 
+                 child:Text(
+                  "Saiu",
+                 style:TextStyle(
+                   fontSize: 16,
+                   color: Colors.black,
+                   style: TextButton.styleFrom(
+                    primary: Colors.black,
+                    backgroundColor:Colors.white,
+                    fixeSized: const Sized(100,100),
+                    shape: RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(16),
+                  ),
+                ),
+               ),
+              ),
+             const SizedBox(
+                width: 32,
+              ),
+            const TextButton(
+                 onPressd: decrement, 
+                 child:Text(
+                  "Entrar",
+                 style:TextStyle(
+                   fontSize: 16,
+                  color: Colors.black,
+                  ),
+                 ),
+              ),
+             ),
+            ],
+           ),    
+         ]
+       ),
+     ),     
+    );     
+    
+  }
+
+
 }
-
-
-
-
-
-
-
-
-
